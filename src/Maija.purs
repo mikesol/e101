f0d34e06aac449cca429af038d1c661b294b72ea
Mike Solomon <mike@meeshkan.com>
2020-11-07 09:21:41 +0200

    more maija

--- src/Maija.purs
+++ src/Maija.purs
@@ -10,7 +10,7 @@ import Data.Profunctor (lcmap)
 import Data.Tuple (Tuple(..))
 import Data.Typelevel.Num (D2)
 import FRP.Behavior (Behavior)
-import FRP.Behavior.Audio (AudioParameter, AudioUnit, evalPiecewise, playBufWithOffset, runInBrowser, speaker)
+import FRP.Behavior.Audio (AudioParameter, AudioUnit, evalPiecewise, playBufWithOffset_, runInBrowser, speaker)
 import Math (pow)
 import Type.Klank.Dev (Klank, defaultEngineInfo, klank, makeBuffersKeepingCache)
 
@@ -28,17 +28,23 @@ kr = (toNumber defaultEngineInfo.msBetweenSamples) / 1000.0 :: Number
 epwf :: Array (Tuple Number Number) -> Number -> AudioParameter
 epwf = evalPiecewise kr
 
-maijaPlayer :: Number -> Number -> List (AudioUnit D2)
-maijaPlayer rate = boundPlayer 2.5 (defer \_ -> pure (playBufWithOffset "maija" rate 4.3))
+maijaChirpPlayer :: String -> Number -> Number -> List (AudioUnit D2)
+maijaChirpPlayer tag rate = boundPlayer (0.5 / rate) (defer \_ -> pure (playBufWithOffset_ tag "maija" rate 1.3))
 
-maijaGliss :: Int -> Array (Number -> List (AudioUnit D2))
-maijaGliss end =
+maijaGlissPlayer :: String -> Number -> Number -> List (AudioUnit D2)
+maijaGlissPlayer tag rate = boundPlayer (2.5 / rate) (defer \_ -> pure (playBufWithOffset_ tag "maija" rate 4.3))
+
+maijaSirenPlayer :: String -> Number -> Number -> List (AudioUnit D2)
+maijaSirenPlayer tag rate = boundPlayer (2.5 / rate) (defer \_ -> pure (playBufWithOffset_ tag "maija" rate 8.2))
+
+maijaGliss :: String -> Int -> Array (Number -> List (AudioUnit D2))
+maijaGliss tag end =
   map
     ( \i ->
         let
           tn = toNumber i
         in
-          atT (0.0 + tn * 0.1) $ maijaPlayer (1.0 + tn * 0.05)
+          atT (0.0 + tn * 0.1) $ maijaGlissPlayer (tag <> show i) (1.0 + tn * 0.05)
     )
     (range 0 (end - 1))
 
@@ -49,7 +55,16 @@ scene time =
         ( zero
             :| ( fold
                   ( map (\f -> f time)
-                      (maijaGliss 7)
+                      ( (map (atT 5.85) (maijaGliss "glissA" 7))
+                          <> [ atT 0.0 $ maijaChirpPlayer "chirpA" 1.9
+                            , atT 1.7 $ maijaChirpPlayer "chirpB" 1.4
+                            , atT 3.4 $ maijaChirpPlayer "chirpC" 1.5
+                            , atT 4.2 $ maijaChirpPlayer "chirpD" 1.2
+                            , atT 5.5 $ maijaChirpPlayer "chirpE" 1.6
+                            ]
+                          <> [ atT 0.0 $ maijaSirenPlayer "sirenA" 0.25
+                            ]
+                      )
                   )
               )
         )
