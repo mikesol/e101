bcf733253b205a3eea2459d0667ff2891e1bc926
Mike Solomon <mike@meeshkan.com>
2020-11-08 17:54:17 +0200

    Better envelope

--- src/Wild.purs
+++ src/Wild.purs
@@ -10,6 +10,7 @@ import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..))
 import Data.Typelevel.Num (D2)
 import Debug.Trace (spy)
+import Effect (Effect)
 import Effect.Random (random)
 import FRP.Behavior (Behavior)
 import FRP.Behavior.Audio (AudioParameter, AudioUnit, convolver_, dup2, evalPiecewise, g'add, g'delay, g'gain, gainT_', gain_', graph, loopBufT_, loopBuf_, playBufWithOffset_, runInBrowser, runInBrowser_, speaker')
@@ -32,11 +33,14 @@ kr = (toNumber defaultEngineInfo.msBetweenSamples) / 1000.0 :: Number
 epwf :: Array (Tuple Number Number) -> Number -> AudioParameter
 epwf = evalPiecewise kr
 
-scene :: Array (Tuple Number Number) -> Number -> Behavior (AudioUnit D2)
-scene a time =
+scene :: Array (Tuple Number Number) -> Array (Tuple Number Number) -> Number -> Behavior (AudioUnit D2)
+scene ramps rates time =
   pure
     $ speaker'
-        (loopBufT_ ("rhythm") "rhythm" (epwf a time) 0.5 4.0)
+        ( gainT_' ("gt")
+            (epwf ramps time)
+            (loopBufT_ ("rhythm") "rhythm" (epwf rates time) 0.5 4.0)
+        )
 
 aggT ::
   Array (Tuple Number Number) ->
@@ -46,28 +50,35 @@ aggT =
     (\{ acc, len } (Tuple n x) -> { acc: acc <> [ (Tuple (n + len) x) ], len: len + n })
     { acc: [], len: 0.0 }
 
+_1k :: Effect (Array (Tuple Number Number))
+_1k =
+  sequence
+    ( replicate 1000
+        ( do
+            l <- random
+            r <- random
+            pure (Tuple l r)
+        )
+    )
+
 main :: Klank
 main =
   klank
     { run =
       runInBrowser_ do
-        rands <-
-          sequence
-            ( replicate 1000
-                ( do
-                    l <- random
-                    r <- random
-                    pure (Tuple l r)
-                )
-            )
+        rates <- _1k
+        ramps <- _1k
         let
-          mapped =
+          ratesM =
             map
               (\(Tuple l r) -> (Tuple (l + 0.03) (0.6 + r * 4.0)))
-              rands
+              rates
         let
-          accd = aggT mapped
-        pure (scene accd.acc)
+          rampsM =
+            map
+              (\(Tuple l r) -> (Tuple (l + 0.03) 0.0))
+              ramps
+        pure (scene (aggT rampsM).acc (aggT ratesM).acc)
     , buffers =
       makeBuffersKeepingCache
         [ Tuple "rhythm" "https://klank-share.s3-eu-west-1.amazonaws.com/e101/test/ryanRhythm.ogg"
